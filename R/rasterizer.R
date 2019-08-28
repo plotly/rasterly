d647deeb025d315e9dc6b1414b5d81d41419ebde
z267xu <40337165+z267xu@users.noreply.github.com>
2019-08-28 17:35:05 -0400

    change meaningless name `canvas` to `rasterizer`

--- R/rasterizer.R
+++ R/rasterizer.R
@@ -1,134 +1,149 @@
+#' @title Rasterizer
+#' @description Set initials for rasterizer
+#' @param data Dataset to use for plot. If not provided, data must be supplied in each layer of the plot.
+#' @param mapping Default list of aesthetic mappings to use for plot. The same with `ggplot2`\link[aes]{aes}.
+#' See details.
+#' @param ... Other arguments can be passed to layer.
+#' @param plot_width The width of image, must be positive integer. Higher value indicates higher resolution.
+#' @param plot_height The height of image, must be positive integer. Higher value indicates higher resolution.
+#' @param x_range The range of x; It can be used to clip the image. Also, to large dataset, provided `x_range`
+#' can help to speed code
+#' @param y_range The range of y; It can be used to clip the image. Also, to large dataset, provided `y_range`
+#' can help to speed code
+#' @param background Background colour of this image. All layers share one background colour.
+#' @param colour_map colour used to map in each pixel. The `colour_map` would be extended by linear interpolation
+#' independently for RGB. The darkness of the colour depends on the aggregation matrix value.
+#' @param colour_key Used for categorical variable. In general, `colour_key` would be called when "colour"
+#' is set in `aes()`
+#' @param show_raster Logical value. Whether show raster or not
+#' @param remove_data Logical value. In general, data passed in has huge size which may take too many rooms. When data is 
+#' manipulated by given `aes()`, original data can be removed via function `remove()` by setting `remove_data = TRUE`. See details 
+#' for more information.
+#' @param variable_check Logical value. Drop unused columns. Setting `TRUE` can help to save more space by sacrificing speed.
+#'
+#' @details 
+#' 1. In `rasterizer`, only five arguments can be passed in `aes()`, "x", "y", "on", "colour" and "size".
+#' variable "on" represents the reduction function works "on" which column. 
+#' 2. `remove_data` can help save space, however, drop original data can cause layers fail to set new `aes()`, 
+#' ex: rasterizer(data, aes(x,y), ..., remove_data = TRUE) %>% rasterize_points() %>% rasterize_points(mapping = aes(x,y,on))
+#' The second layer fail to set new `aes()` since the original data is missing, only manipulated dataframe is passed through.
+#' 
+#'
+#' @useDynLib rasterizer
+#' @import Rcpp
+#' @import rlang
+#' @importFrom grDevices rgb col2rgb hcl extendrange
+#' @importFrom magrittr '%>%'
+#' @importFrom stats ecdf approx setNames
+#' @importFrom ggplot2 aes
+#' @importFrom data.table data.table
+#' @importFrom compiler cmpfun
+#'
+#'
+#' @examples
+#' \dontrun{
+#'    library(rasterizer)
+#'    if(requireNamespace("grid") && requireNamespace("gridExtra")) {
+#'      x <- rnorm(1e7)
+#'      y <- rnorm(1e7)
+#'      category <- sample(1:5, 1e7, replace = TRUE)
+#'      data.frame(x = x, y = y, category = category) %>%
+#'        rasterizer(mapping = aes(x = x, y = y, colour = category)) %>%
+#'        rasterize_points(layout = "weighted") %>%
+#'        execute() -> ds1
+#'
+#'      data.frame(x = x, y = y, category = category) %>%
+#'        rasterizer(mapping = aes(x = x, y = y, colour = category)) %>%
+#'        rasterize_points(layout = "cover") %>%
+#'        execute() -> ds2
+#'
+#'      grid::grid.newpage()
+#'      gridExtra::grid.arrange(
+#'         grobs = list(grid::rasterGrob(ds1$image), grid::rasterGrob(ds2$image)),
+#'         ncol = 2,
+#'         top = "'weighted' layout versus 'cover' layout"
+#'      )
+#'    }
+#' }
+
 #' @export
-rasterizer <- function(rastObj) {
+rasterizer <- function(data = NULL,
+                   mapping = aes(),
+                   ...,
+                   plot_width = 600, plot_height = 600,
+                   x_range = NULL, y_range = NULL,
+                   background = "#FFFFFF",
+                   colour_map = c('lightblue','darkblue'),
+                   colour_key = NULL,
+                   show_raster = TRUE,
+                   remove_data = FALSE,
+                   variable_check = FALSE) {
+  # argument check
+  if(!missing(mapping) && !inherits(mapping, "uneval")) {
+    stop("Mapping should be created with `aes()`.", call. = FALSE)
+  }
   
-  if(missing(rastObj) || !is.rasterizer(rastObj)) stop("No 'rasterizer' object", call. = FALSE)
-  if(!is.aggregation(rastObj)) stop("No 'aggregation' layer", call. = FALSE)
-  
-  canvas_env <- rastObj[["canvas_env"]]
-  rastObj[["canvas_env"]] <- NULL
-  aggregation_env <- rastObj
-  remove(rastObj)
-  
-  plot_width <- .get("plot_width", envir = canvas_env)
-  plot_height <- .get("plot_height", envir = canvas_env)
-  x_range <- .get("x_range", envir = canvas_env)
-  y_range <- .get("y_range", envir = canvas_env)
-  show_raster <- .get("show_raster", envir = canvas_env)
-  
-  # modify range
-  lims <- lapply(aggregation_env, 
-                 function(envir) {
-                   xlim <- get("xlim", envir = envir, inherits = FALSE)
-                   ylim <- get("ylim", envir = envir, inherits = FALSE)
-                   # update ranges
-                   x_range <<- range(x_range, xlim)
-                   y_range <<- range(y_range, ylim)
-                   
-                   list(
-                     xlim = xlim, 
-                     ylim = ylim
-                   )
-                 })
-  variable_names <- lapply(aggregation_env, 
-                           function(envir) {
-                             get("variable_names", envir = envir, inherits = FALSE)
-                           })
-  # used for lapply scopping
-  # if show_raster is TRUE
-  image <- NULL
-  bg <- c()
-  colours <- list()
-  agg <- lapply(aggregation_env, 
-                function(envir) {
-                  # "aggregation" is a list of matrices modified by some specific reduction function
-                  # if the length of "aggregation" is one, which means it is a single image
-                  # else it should be multiple images 
-                  xlim <- get("xlim", envir = envir, inherits = FALSE)
-                  ylim <- get("ylim", envir = envir, inherits = FALSE)
-                  aesthetics <- get("aesthetics", 
-                                    envir = envir, 
-                                    inherits = FALSE)
-                  if(is.null(aesthetics)) aesthetics <- get("aesthetics", 
-                                                            envir = canvas_env, 
-                                                            inherits = FALSE)
-                  
-                  start_time <- Sys.time()
-                  agg <- get_aggregation(
-                    plot_width = plot_width, 
-                    plot_height = plot_height,
-                    aesthetics = aesthetics,
-                    x_range = x_range, y_range = y_range, 
-                    xlim = xlim, ylim = ylim,
-                    func = get("reduction_func", envir = envir, inherits = FALSE),
-                    glyph = get("glyph", envir = envir, inherits = FALSE),
-                    group_by_data_table = get("group_by_data_table", envir = envir, inherits = FALSE)
-                  )
-                  end_time <- Sys.time()
-                  print(paste("get_aggregation time:", end_time - start_time))
-                  
-                  len_agg <- length(agg)
-                  if(len_agg == 0) stop("No aggregation matrices are found", call. = FALSE)
-                  else if(len_agg == 1) {
-                    # agg is a matrix
-                    agg <- agg[[1]]
-                    class(agg) <- c("rasterizerMatrix", "rasterizer", "matrix")
-                    colour <- get("colour_map", 
-                                  envir = envir, 
-                                  inherits = FALSE)
-                  } else {
-                    # agg is a list
-                    agg <- lapply(agg, 
-                                  function(a) {
-                                    class(a) <- c("rasterizerMatrix", "rasterizer", "matrix")
-                                    a
-                                  })
-                    class(agg) <- c("rasterizerList", "rasterizer", "list")
-                    colour <- get_colour_key(
-                      colour_key = get("colour_key", 
-                                       envir = envir, 
-                                       inherits = FALSE), 
-                      n = len_agg,
-                      canvas_colour_key = get("colour_key", 
-                                              envir = canvas_env, 
-                                              inherits = FALSE)
-                    )
-                  }
-                  
-                  # show raster or not
-                  if(show_raster) {
-                    start_time <- Sys.time()
-                    background <- get("background", envir = envir, inherits = FALSE)
-                    bg <<- c(bg, background)
-                    colours <<- c(colours, list(colour))
-                    image <<- as.raster(x = agg,    
-                                        colour = colour,          
-                                        span = get("span", envir = envir, inherits = FALSE),
-                                        zeroIgnored = TRUE, 
-                                        image = image, 
-                                        background = background,
-                                        alpha = get("alpha", envir = envir, inherits = FALSE),
-                                        layout = get("layout", envir = envir, inherits = FALSE))
-                    end_time <- Sys.time()
-                    print(paste("get raster time:", end_time - start_time))
-                  }
-                  
-                  if(inherits(agg, "rasterizerMatrix")) agg <- list(agg)
-                  agg
-                }
+  stopifnot(
+    exprs = {
+      is.numeric(plot_width) && plot_width > 0
+      is.numeric(plot_height) && plot_height > 0
+    }
   )
+  plot_width <- round(plot_width)
+  plot_height <- round(plot_height)
   
-  l <- list(
-    agg = agg, 
-    image = image,
-    lims = lims,
-    x_range = x_range, 
-    y_range = y_range,
-    plot_height = plot_height,
-    plot_width = plot_width,
-    variable_names = variable_names,
-    background = bg,
-    colours = colours
+  if(!is.null(x_range)) {
+    stopifnot(
+      exprs = {
+        length(x_range) == 2 && is.numeric(x_range)
+      }
+    )
+  }
+  
+  if(!is.null(y_range)) {
+    stopifnot(
+      exprs = {
+        length(y_range) == 2 && is.numeric(y_range)
+      }
+    )
+  }
+  
+  if(!is.null(data) && !rlang::is_empty(mapping)) {
+    
+    start_time <- Sys.time()
+    aesthetics <- get_aesthetics(data, mapping, variable_check, ...)
+    end_time <- Sys.time()
+    print(paste("get aesthetics time:", end_time - start_time))
+    
+    start_time <- Sys.time()
+    range <- get_range(x_range = x_range, 
+                       y_range = y_range,
+                       x = aesthetics$x, 
+                       y = aesthetics$y)
+    x_range <- range$x_range
+    y_range <- range$y_range
+    end_time <- Sys.time()
+    print(paste("get range time:", end_time - start_time))
+    
+    if(remove_data) {
+      remove(data)
+      message("The layer with new mapping aesthetics may do not work")
+    }
+  }
+  
+  # make sure all arguments exist in `rasterizer` environment
+  args <- list(...)
+  reduction_func <- args$reduction_func
+  layout <- args$layout
+  glyph <- args$glyph
+  group_by_data_table <- args$group_by_data_table
+  
+  p <- structure(
+    list(
+      rasterizer_env = environment()
+    ),
+    class = c("rasterizer")
   )
-  class(l) <- "rasterizer"
-  return(l)
+  return(p)
+  invisible()
 }
