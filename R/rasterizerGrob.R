23cd10c8d39a40a326bfcc6b7285cf3c15f43095
z267xu <z267xu@github.com>
2019-08-30 12:08:20 -0400

    documentation for `grid.rasterizer`

--- R/rasterizerGrob.R
+++ R/rasterizerGrob.R
@@ -1,24 +1,70 @@
+#' @title Render a "rasterizer" object
+#' @description Render a "rasterizer" object 
+#' @param rastObj A "rasterizer" object
+#' @param ... To specify layout aesthetics. See more in details
+#' @param interpolate A logical value indicating whether to linearly interpolate the image 
+#' (the alternative is to use nearest-neighbour interpolation, which gives a more blocky result).
+#' @param name A character identifier.
+#' @param gp An object of class gpar, typically the output from a call to the function gpar. 
+#' This is basically a list of graphical parameter settings.
+#' @param vp A Grid viewport object (or NULL).
+#' @details
+#' Set ... in `grid.rasterizer()` and `rasterizerGrob()`
+#' \itemize{
+#'  \item{}{title: The title of output graph}
+#'  \item{}{title_fontsize: title fontsize}
+#'  \item{}{title_fontfamily: title fontfamily}
+#'  \item{}{title_fontface: title fontface}
+#'  \item{}{title_location: title location}
+#'  \item{}{title_hvjust: title hvjust, a length 2 vector represents "hjust" and "vjust" correspondingly}
+#'  \item{}{title_col: title colour}
+#'  \item{}{xlabel: x label, default setting is the "x" name in mapping system}
+#'  \item{}{ylabel: y label, default setting is the "y" name in mapping system}
+#'  \item{}{label_fontsize: label fontsize}
+#'  \item{}{label_fontfamily: label fontfamily}
+#'  \item{}{label_fontface: label fontface}
+#'  \item{}{label_location: label location}
+#'  \item{}{label_hvjust: label hvjust, a length 2 vector represents "hjust" and "vjust" correspondingly}
+#'  \item{}{label_col: label colour}
+#'  \item{}{axis_col: axis colour}
+#'  \item{}{axis_fontsize: axis fontsize}
+#'  \item{}{image_width: image width}
+#'  \item{}{image_height: image height}
+#' }
+#' @usage 
+#' grid.rasterizer(rastObj, ..., interpolate = FALSE,
+#'   name = NULL, gp = grid::gpar(), vp = NULL)
+#'     
+#' rasterizerGrob(rastObj, ..., interpolate = FALSE, 
+#'   name = NULL, gp = grid::gpar(), vp = NULL)
 #' @export
 #' @import grid
+#' @examples 
+#' iris %>% 
+#'   rasterizer(plot_height = 20, 
+#'              plot_width = 20, 
+#'              mapping = aes(x = Sepal.Width, y = Petal.Length, colour = Species)) %>% 
+#'   rasterize_points() %>% 
+#'   execute() %>% 
+#'   grid.rasterizer()
 
-grid.rasterizer <- function (widget, ..., name = NULL, gp = grid::gpar(), draw = TRUE, vp = NULL) {
+grid.rasterizer <- function (rastObj, ..., interpolate = FALSE, 
+                             name = NULL, gp = grid::gpar(), vp = NULL) {
   
-  lg <- rasterizerGrob(widget, ..., name = name, gp = gp, vp = vp)
+  lg <- rasterizerGrob(rastObj, ..., interpolate = interpolate, 
+                       name = name, gp = gp, vp = vp)
   
-  if (draw)
-    grid::grid.draw(lg)
-  
-  invisible(lg)
+  grid::grid.draw(lg)
 }
 
 #' @export
-plot.rasterizer <- function (x, y = NULL, ...) {
-  if (!is.null(y)) warning("argument y is ignored")
-  g <- grid.rasterizer(x, ...)
-  invisible(g)
-}
-
-#' @export
+#' @inherit grid.rasterizer
+#' @usage 
+#' grid.rasterizer(rastObj, ..., interpolate = FALSE,
+#'   name = NULL, gp = grid::gpar(), vp = NULL)
+#'     
+#' rasterizerGrob(rastObj, ..., interpolate = FALSE, 
+#'   name = NULL, gp = grid::gpar(), vp = NULL)
 rasterizerGrob <- function(rastObj, ..., interpolate = FALSE,
                            name = NULL, gp = grid::gpar(), vp = NULL) {
   
@@ -53,6 +99,9 @@ rasterizerGrob <- function(rastObj, ..., interpolate = FALSE,
   # axis 
   axis_col <- args$axis_col %||% "black"
   axis_fontsize <- args$axis_fontsize %||% 8
+  # image 
+  image_width <- args$image_width %||% grid::unit(1.01, "npc")
+  image_height <- args$image_height %||% grid::unit(1.01, "npc")
 
   rGrob <- grid::gTree(
     children = grid::gList(
@@ -137,8 +186,8 @@ rasterizerGrob <- function(rastObj, ..., interpolate = FALSE,
           grid::rasterGrob(
             image = rastObj$image,
             interpolate = interpolate,
-            width = grid::unit(1.01, "npc"),
-            height = grid::unit(1.01, "npc"),
+            width = image_width,
+            height = image_height,
             name = "image"
           ),
           grid::clipGrob(name = "clipping region")
@@ -154,4 +203,33 @@ rasterizerGrob <- function(rastObj, ..., interpolate = FALSE,
     ),
     name = name, vp = vp, gp = gp
   )
+}
+
+
+#' Plot "rasterizer" object.
+#'
+#' This is a wrapper for \code{grid.rasterizer()} to simplify the plotting.
+#'
+#' @param x a "rasterizer" object to be plotted
+#' @param y NULL, will be ignored.
+#' @param ... parameters passed to \code{grid.rasterizer} and \code{rasterizerGrob}
+#'
+#' @return invisible()
+#'
+#'
+#' @seealso \code{\link{grid.rasterizer}}, \code{\link{rasterizerGrob}}
+#'
+#' @examples
+#' mtcars %>% 
+#'   rasterizer(plot_height = 20, 
+#'              plot_width = 20, 
+#'              mapping = aes(x = hp, y = mpg, colour = cyl)) %>% 
+#'   rasterize_points() %>% 
+#'   execute() -> p
+#'   plot(x = p)
+plot.rasterizer <- function (x, y = NULL, ...) {
+  if (!is.null(y)) warning("argument y is ignored")
+  grid::grid.newpage()
+  g <- grid.rasterizer(x, ...)
+  invisible(g)
 }
