26500a11638959a8c33b43b89a0785252477093f
z267xu <z267xu@github.com>
2019-09-06 09:45:16 -0400

    add reduction functions

---
+++ R/get_aggregation_sd.R
@@ -0,0 +1,20 @@
+get_aggregation.var <- function(plot_width, plot_height, aesthetics,
+                                x_range, y_range, xlim, ylim, 
+                                func, glyph, group_by_data_table, ...) {
+  
+  if(is.null(aesthetics$on)) stop("No `on` argument. Which variable is used for sum of square differences from the mean?")
+  is_size <- !is.null(aesthetics$size)
+  is_colour <- !is.null(aesthetics$colour)
+  
+  aesthetics$on <- stats::sd(aesthetics$on)
+  get_aggregation.sum(plot_width = plot_width,
+                      plot_height = plot_height, 
+                      aesthetics = aesthetics,
+                      x_range = x_range, 
+                      y_range = y_range, 
+                      xlim = xlim, 
+                      ylim = ylim, 
+                      func = NULL, 
+                      glyph = glyph, 
+                      group_by_data_table = group_by_data_table, ...)
+}
