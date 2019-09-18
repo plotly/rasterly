b02949ac0bed3758b260accf4ff2628852268616
z267xu <z267xu@github.com>
2019-09-18 14:54:17 -0400

    `grid.newpage` is called when we call `plot.rasterizer` function

--- R/print.R
+++ R/print.R
@@ -2,6 +2,7 @@
 print.rasterizer <- function(rastObj) {
   
   if(is.rasterizeLayer(rastObj) && !is.rasterizer_build(rastObj)) rastObj <- rasterizer_build(rastObj)
+  grid::grid.newpage()
   grid.rasterizer(rastObj)
   invisible()
 }
